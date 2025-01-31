bdc77078db4622abd71b2e9756cb7765274891ed
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-01-31 11:31:09 +0100

    Configuración de las rutas de los componentes

--- proyecto-angular/src/app/app.routes.ts
+++ proyecto-angular/src/app/app.routes.ts
@@ -13,3 +13,17 @@ import { CreateComponent } from './components/create/create.component';
 import { ContactComponent } from './components/contact/contact.component';
 
 
+// Definir las rutas: 
+
+const appRoutes: Routes = [
+
+    {path: '', component: AboutmeComponent},
+    {path: 'About Me', component: AboutmeComponent},
+    {path: 'Projects', component: ProjectsComponent},
+    {path: 'Create Project', component: CreateComponent},
+    {path: 'Contact me', component: ContactComponent},
+    {path: '**', component: AboutmeComponent},
+
+];
+
+
