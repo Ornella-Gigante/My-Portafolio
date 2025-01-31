b5931fb18c94026be0a79ccf81c40ae9a4e413be
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-01-31 11:25:30 +0100

    Importaciones en routes.ts

--- proyecto-angular/src/app/app.routes.ts
+++ proyecto-angular/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+
+import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [];
+
+import { AboutmeComponent} from './components/aboutme/aboutme.component';
+
+import { ProjectsComponent } from './components/projects/projects.component';
+
+import { CreateComponent } from './components/create/create.component';
+
+import { ContactComponent } from './components/contact/contact.component';
+
+
